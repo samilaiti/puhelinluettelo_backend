33d0b5cd9aec333d1e3bac07901fb152ad3d84f6
Sami Laitinen <sami.p.laitinen@gmail.com>
2024-12-11 16:46:50 +0200

    CORS added and delete edited

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :param'))
+app.use(cors())
 
 morgan.token('param', function(req, res, param) {
   if (req.method === "POST") {
@@ -62,7 +64,9 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
+  const person = persons.filter(person => person.id === id)
   persons = persons.filter(person => person.id !== id)
+  response.json(person[0])
   response.status(204).end()
 })
 
