edf9561e908ac0e08e8a72ba769a33172021409f
Sami Laitinen <sami.p.laitinen@gmail.com>
2024-12-11 17:08:36 +0200

    front-end build added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :param'))
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('param', function(req, res, param) {
   if (req.method === "POST") {
